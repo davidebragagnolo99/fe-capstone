d8200ae22783146bcefec2d26475f06336ba9986
davide bragagnolo <davidebraga.99@gmail.com>
2023-12-19 16:59:14 +0100

    add components and image

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
 
 export const Shop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+      <Popular />
+    </div>
+  );
 };
 
 export default Shop;
