1fc584e43a682084eb48afcc5114488112eb1a79
davide bragagnolo <davidebraga.99@gmail.com>
2023-12-21 12:37:34 +0100

    add

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ const ShopCategory = (props) => {
   // console.log(props, all_product);
   return (
     <div className="shop-category">
-      <img src={props.banner} alt="" />
+      <img className="shopcategory-banner" src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -38,6 +38,7 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
+      <div className="shopcategory-loadmore">Explore More</div>
     </div>
   );
 };
